ee0e828e918916d8ad363fe94ec0b6fcfa6d7ae5
dranaconda <anonymouls777@gmail.com>
2021-05-01 23:38:23 +0300

    Extended SMConfiguration.cs Integrated parser

--- ASM.SMFaker/Configuration/SMConfiguration.cs
+++ ASM.SMFaker/Configuration/SMConfiguration.cs
@@ -2,6 +2,7 @@
 {
     public class SMConfiguration
     {
+        public bool readFromEnv { get; set; } = false;
         public string username { get; set; }
         public string password { get; set; }
         public string baseUrl { get; set; } = "https://www.screenmeter.com/api";
